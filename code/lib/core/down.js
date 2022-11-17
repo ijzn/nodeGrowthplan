09acc15204e58d6cbb506b3aa004873fa041c753
592193834@qq.com <592193834@qq.com>
2022-11-17 17:47:17 +0800

    feat: 删除冗余代码

--- code/lib/core/down.js
+++ code/lib/core/down.js
@@ -2,16 +2,10 @@ const download = require("download-git-repo");
 const ora = require("ora");
 const chalk = require("chalk");
 const log = console.log;
-// const git = require("simple-git/promise");
 
 const downloaAction = async (project, url) => {
   const spinner = ora("项目初始化...").start();
-  // spinner.succeed("下载成功");
-  // const GIT_REPOSITORY_ROOT = process.cwd();
-  // const simpleGit = git(GIT_REPOSITORY_ROOT);
-  // simpleGit.clone(url)
   download(`direct:${url}`, project, { clone: true }, function (err) {
-    // console.log("download", err);
     if (err) {
       spinner.fail("下载失败");
       log(err);
